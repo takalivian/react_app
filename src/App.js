ba5861e13bd81241a21afeb4448652dd641983aa
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-18 23:05:00 +0900

    リストコンポーネント

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 
 class App extends Component {
 
-  date =[];
+  date =[
+    "This is list sample.",
+    "これはリストのサンプルです。",
+    "配列をリストに変換します。"
+  ];
 
   msgStyle = {
     fontSize:"20pt",
@@ -13,51 +17,68 @@ class App extends Component {
     padding:"5px"
   }
 
-  area= {
-    width:"500px",
-    height:"500px",
-    border: "1px solid blue"
-  }
-
   constructor(props){
     super(props);
     this.state={
       list:this.date
     };
-    this.doAction = this.doAction.bind(this);
   }
 
-  doAction(e){
-    let x = e.pageX;
-    let y = e.pageY;
-    this.date.push({x:x, y:y});
-    this.setState({
-      list:this.date
-    }) ;
-  }
-
-  draw(d){
-    let s = {
-      position:"absolute",
-      left:(d.x - 25) + "px",
-      top:(d.y - 25) + "px",
-      width:"50px",
-      height:"50px",
-      backgroundColor:"#66f3"
-    };
-    return <div style={s} ></div>;
-  }
-
-  render (){
+  render(){
     return <div>
-      <h1>react</h1>
-      <h2 style={this.msgStyle}>show rect.</h2>
-      <div style={this.area} onClick={this.doAction}>
-        {this.date.map((value)=>this.draw(value))}
-      </div>
+      <h1>React</h1>
+      <h2 style ={this.msgStyle}>show list.</h2>
+      <List title= "サンプル・リスト" date={this.date} />
     </div>;
   }
+}
 
+class List extends Component {
+  number = 1;
+
+  title = {
+    fontSize:"20pt",
+    fontWight:"bold",
+    color:"blue"
+  }
+
+  render(){
+    let date = this.props.date;
+      return (
+        <div>
+          <p style={this.title}>{this.props.title}</p>
+            <ul>
+              {date.map((item)=>
+                <Item number={this.number++} value={item}
+                  key={this.number} />
+              )}
+            </ul>
+        </div>
+      )
+  }
+}
+
+class Item extends Component {
+  li = {
+    listStyleType:"square",
+    fontSize:"16pt",
+    color:"#06",
+    margin:"0",
+    padding: "0"
+  }
+  num = {
+    fontWeight:"bold",
+    color:"red"  
+  }
+
+  render(){
+    return (
+      <li style={this.li}>
+        <span style={this.num}>[{this.props.number}]</span>
+        {this.props.value}
+      </li>
+    )
+  }
 }
 
 export default App;
