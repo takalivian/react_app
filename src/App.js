abe3e0c976c91217e33e2a495ea46c5b561ffd7a
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-24 23:47:15 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,85 +1,38 @@
 import React, { Component } from 'react';
-// import Rect from './Rect';
-import './App.css';
 import { connect } from 'react-redux';
+import './App.css';
+import Memo from './memo/Memo';
+import AddForm from './memo/AddForm';
+import FindForm from './memo/FindForm';
+import DelForm from './memo/DelForm';
 
-// ステートのマッピング
-function mappingStare(state) {
-  return state;
-}
 
 // Appコンポーネント
 class App extends Component {
-
-  constructor(props) {
-    super(props);
+  td = {
+    width:"250px"
   }
 
-  render() {
-    return(
-      <div>
-        <h1>Redux</h1>
-        <Message />
-        <Button />
-      </div>
-    );
-  }
-
-}
-
-// ストアのコネクト
-App = connect()(App);
-
-// メッセージ表示のコンポーネント
-class Message extends Component {
-  style = {
-    fontSize: "20pt",
-    padding: "20px 5px"
-  }
-
-  render() {
-    return(
-      <p  style={this.style}>
-        {this.props.message}: {this.props.counter}
-      </p>
-    );
-  }
-}
-
-// ストアのコネクト
-Message = connect(mappingStare)(Message);
-
-class Button extends Component {
-  style ={
-    fontSize: "16pt",
-    padding: "5px 10px"
-  }
 
   constructor(props){
     super(props);
-    this.doAction = this.doAction.bind(this);
   }
 
-  // ボタンクリックでディスパッチを実行
-  doAction(e){
-    if (e.shiftKey) {
-      this.props.dispatch({ type:"DECREMENT"});
-    } else {
-      this.props.dispatch({ type:"INCREMENT"});
-    }
-  }
-
-  render(){
+  render() {
     return (
-      <button style={this.style} onClick={this.doAction}>
-        click
-      </button>
-    )
+      <div>
+        <h1>Memo</h1>
+        <AddForm />
+        <hr />
+        <table><tbody><tr>
+          <td style={this.td}><FindForm /></td>
+          <td style={this.td}><DelForm /></td>
+        </tr></tbody></table>
+        <Memo />
+      </div>
+    );
   }
-
 }
 
-// ストアのコネクト
-Button = connect()(Button);
 
-export default App;
+export default connect()(App);
